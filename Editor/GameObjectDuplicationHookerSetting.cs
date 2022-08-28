7164ef20982e7e3359fdb0e7d1fa79ec1e3f4263
baba-s <shotababa1206@gmail.com>
2022-08-28 14:46:43 +0900

    update

---
+++ Editor/GameObjectDuplicationHookerSetting.cs
@@ -0,0 +1,35 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Kogane.Internal
+{
+    /// <summary>
+    /// 独自のゲームオブジェクト複製機能の設定を管理するクラス
+    /// </summary>
+    [FilePath( "UserSettings/Kogane/GameObjectDuplicationHooker.asset", FilePathAttribute.Location.ProjectFolder )]
+    internal sealed class GameObjectDuplicationHookerSetting : ScriptableSingleton<GameObjectDuplicationHookerSetting>
+    {
+        //================================================================================
+        // 変数(SerializeField)
+        //================================================================================
+        [SerializeField] private bool m_isEnable;                    // 有効な場合 true
+        [SerializeField] private bool m_isEnableSerialNumber = true; // 文字列末尾の数値の連番対応を有効化する場合 true
+
+        //================================================================================
+        // プロパティ
+        //================================================================================
+        public bool IsEnable             => m_isEnable;
+        public bool IsEnableSerialNumber => m_isEnableSerialNumber;
+
+        //================================================================================
+        // 関数
+        //================================================================================
+        /// <summary>
+        /// 保存します
+        /// </summary>
+        public void Save()
+        {
+            Save( true );
+        }
+    }
+}
---
+++ Editor/GameObjectDuplicationHookerSettingProvider.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Kogane.Internal
+{
+    /// <summary>
+    /// 独自のゲームオブジェクト複製機能の Project Settings における設定画面を管理するクラス
+    /// </summary>
+    internal sealed class GameObjectDuplicationHookerSettingProvider : SettingsProvider
+    {
+        //================================================================================
+        // 定数
+        //================================================================================
+        private const string PATH = "Kogane/Game Object Duplication Hooker";
+
+        //================================================================================
+        // 変数
+        //================================================================================
+        private Editor m_editor;
+
+        //================================================================================
+        // 関数
+        //================================================================================
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        private GameObjectDuplicationHookerSettingProvider
+        (
+            string              path,
+            SettingsScope       scopes,
+            IEnumerable<string> keywords = null
+        ) : base( path, scopes, keywords )
+        {
+        }
+
+        /// <summary>
+        /// アクティブになった時に呼び出されます
+        /// </summary>
+        public override void OnActivate( string searchContext, VisualElement rootElement )
+        {
+            var instance = GameObjectDuplicationHookerSetting.instance;
+
+            instance.hideFlags = HideFlags.HideAndDontSave & ~HideFlags.NotEditable;
+
+            Editor.CreateCachedEditor( instance, null, ref m_editor );
+        }
+
+        /// <summary>
+        /// GUI を描画する時に呼び出されます
+        /// </summary>
+        public override void OnGUI( string searchContext )
+        {
+            using var changeCheckScope = new EditorGUI.ChangeCheckScope();
+
+            m_editor.OnInspectorGUI();
+
+            if ( !changeCheckScope.changed ) return;
+
+            GameObjectDuplicationHookerSetting.instance.Save();
+        }
+
+        //================================================================================
+        // 関数(static)
+        //================================================================================
+        /// <summary>
+        /// Project Settings にメニューを追加します
+        /// </summary>
+        [SettingsProvider]
+        private static SettingsProvider CreateSettingProvider()
+        {
+            return new GameObjectDuplicationHookerSettingProvider
+            (
+                path: PATH,
+                scopes: SettingsScope.Project
+            );
+        }
+    }
+}
